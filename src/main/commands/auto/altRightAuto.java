f1fb52df3d1ebfc7470ca7373d8c510a9bcd2c78
AlexanderPeters <Alexander the geek@192.168.2.2>
2017-02-28 19:35:55 -0500

    Preparing other testable auto options for palmetto

---
+++ src/main/commands/auto/altRightAuto.java
@@ -0,0 +1,22 @@
+package main.commands.auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import main.Constants;
+import main.commands.drivetrain.DriveDistance;
+import main.commands.drivetrain.TimedDrive;
+import main.commands.drivetrain.TurnToHeading;
+import main.commands.gearmech.GearDown;
+import main.commands.gearmech.GearUp;
+
+public class altRightAuto extends CommandGroup implements Constants {
+	public altRightAuto() {
+		addSequential(new DriveDistance(3.75, kToleranceDisplacementDefault));
+		addSequential(new TurnToHeading(-45, 0.5));//Better turning tolerance
+		addSequential(new DriveDistance(4.82, kToleranceDisplacementDefault));
+		//addSequential(new TurnToHeading(0, kToleranceDegreesDefault)); Use if you need to reset and then run DriveDisplacement again
+		addSequential(new GearDown());
+		addSequential(new TimedDrive(0.4, 2));
+		addSequential(new GearUp());
+	}
+
+}
